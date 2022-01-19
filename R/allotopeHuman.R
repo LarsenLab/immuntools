a6d89e557629bcf5461660a035bdd8eba02c5adb
xzhan50 <joan.zhang@emory.edu>
2022-01-19 15:18:18 -0500

    update

--- R/allotopeHuman.R
+++ R/allotopeHuman.R
@@ -5,6 +5,7 @@
 #' @param present_ag presenting alleles.
 #' @param seq_len sequence length.
 #' @param out_name name and path of html report.
+#' @param align_plot whether or not plot alignment plots, default is FALSE.
 #' @return
 #' an html report
 #' @export
@@ -17,14 +18,16 @@
 #' self_ag = c("DQB1_03_01", "DQB1_03_01"),
 #' present_ag = c("DRB1_08_03", "DRB1_04_01"),
 #' seq_len = '15',
-#' out_name = '~/projects/allotope_report.html')
+#' out_name = '~/projects/allotope_report.html'),
+#' align_plot = FALSE
 #' }
 
 allotope_human <- function(don_ag = c("DQB1_05_01", "DQB1_06_02"),
                            self_ag = c("DQB1_03_01", "DQB1_03_01"),
                            present_ag = c("DRB1_08_03", "DRB1_04_01"),
                            seq_len = '15',
-                           out_name = 'allotope_report.html')
+                           out_name = 'allotope_report.html',
+                           align_plot = FALSE)
 {
   render(paste(path.package("immuntools"),"rmarkdown/templates/report/allotope/allotopeHuman.Rmd",sep = "/"),
                     params = list(donor_ag = don_ag,
